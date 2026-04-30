b08ffd2af842f606a2fc646acd984614f172abf6
Ashwin Bolne <ashwinbolne610cs@gmail.com>
2026-04-30 17:28:35 +0530

    feat: add feature engineering step (family_size, age_group) using assign

--- pandas/pandas_pipeline.py
+++ pandas/pandas_pipeline.py
@@ -1,6 +1,10 @@
 import seaborn as sns
 import pandas as pd
 
+AGE_BINS = [0, 18, 60, 100]
+AGE_LABELS = ["child", "adult", "senior"]
+
+
 def drop_high_null_columns(df: pd.DataFrame, thresold: float = 0.5) -> pd.DataFrame:
     """
     Drops columns where null percentage exceeds thresold
@@ -39,6 +43,25 @@ def fill_categorical_nulls(df: pd.DataFrame) -> pd.DataFrame:
 
     return df.fillna(fill_values)
 
+def add_features(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Add engineered features:
+    - family_size
+    - age_group 
+    """
+
+    return (
+        df
+        .assign(
+            family_size=lambda x: x["sibsp"] + x["parch"],
+            age_group=lambda x: pd.cut(
+                x["age"],
+                bins=AGE_BINS,
+                labels=AGE_LABELS
+            )
+        )
+    )
+   
 def run_pipeline(df: pd.DataFrame) -> pd.DataFrame:
     """
     Runs full pandas transformation pipeline
@@ -48,11 +71,14 @@ def run_pipeline(df: pd.DataFrame) -> pd.DataFrame:
         .pipe(drop_high_null_columns, thresold=0.5)
         .pipe(fill_numeric_nulls, strategy="median")
         .pipe(fill_categorical_nulls)
+        .pipe(add_features)
     )
 
 if __name__ == "__main__":
-    
+    import seaborn as sns
+
     df = sns.load_dataset("titanic")
     df_clean = run_pipeline(df)
 
+    print(df_clean[["sibsp", "parch", "family_size", "age", "age_group"]].head())
     print(df_clean.isnull().sum())
